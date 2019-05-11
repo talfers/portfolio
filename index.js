19ff44da7963c672a3d6068fa5b1945eb2a38c52
Taylor Alfers <Taylor>
2019-05-11 09:26:32 -0600

    index page with sass

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/', (req, res)=>{
+  res.sendFile('index.html');
+})
+
+app.listen(port, ()=>{
+  console.log('Server started on port ' + port);
+})
